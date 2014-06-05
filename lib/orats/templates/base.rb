cc7c6069dc5445da82f4128f43496d180bde07a8
Nick Janetakis <nick.janetakis@gmail.com>
2014-06-05 10:40:50 -0400

    Change the secrets.yml file by DRYing it out

--- lib/orats/templates/base.rb
+++ lib/orats/templates/base.rb
@@ -132,10 +132,36 @@ end
 
 # ----- Modify the secrets yaml file -----------------------------------------------------------------------
 
-env_rails_secret_token = "<%= ENV['TOKEN_RAILS_SECRET'] %>"
+gsub_file 'config/secrets.yml', /.*\n/, ''
+append_file 'config/secrets.yml' do <<-FILE
+# Be sure to restart your server when you modify this file.
 
-gsub_file 'config/secrets.yml', /\w{128}/, env_rails_secret_token
-gsub_file 'config/secrets.yml', '<%= ENV["SECRET_KEY_BASE"] %>', env_rails_secret_token
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure the secrets in this file are kept private
+# if you're sharing your code publicly.
+
+development: &default
+  secret_key_base: <%= ENV['TOKEN_RAILS_SECRET'] %>
+
+test:
+  <<: *default
+
+staging:
+  <<: *default
+
+production:
+  <<: *default
+FILE
+end
+
+git add:    '-A'
+git commit: "-m 'Dry up the secrets settings'"
 
 # ----- Modify the application file -------------------------------------------------------------------
 
