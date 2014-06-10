a51379323976163873311377a472a714ed97c6cf
Nick Janetakis <nick.janetakis@gmail.com>
2014-06-10 14:54:16 -0400

    Extract each task to a function for easier maintenance

--- lib/orats/templates/play.rb
+++ lib/orats/templates/play.rb
@@ -1,30 +1,28 @@
-# =====================================================================================================
-# Template for generating an orats ansible 1.6.x playbook
-# =====================================================================================================
-
 require 'securerandom'
 
+# =============================================================================
+# template for generating an orats ansible playbook for ansible 1.6.x
+# =============================================================================
+# view the task list at the bottom of the file
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# private functions
+# -----------------------------------------------------------------------------
 def generate_token
   SecureRandom.hex(64)
 end
 
-def log_task(message)
-  puts
-  say_status  'task', "#{message}...", :yellow
-  puts        '-'*80, ''; sleep 0.25
+def method_to_sentence(method)
+  method.tr!('_', ' ')
+  method[0] = method[0].upcase
+  method
 end
 
-def log_complete
+def log_task(message)
   puts
-  say_status  'success', "\e[1m\Everything has been setup successfully\e[0m", :cyan
-  puts
-  say_status  'question', 'Are most of your apps similar?', :yellow
-  say_status  'answer', 'You only need to generate one playbook and you just did', :white
-  say_status  'answer', 'Use the inventory in each project to customize certain things', :white
-  puts
-  say_status  'question', 'Are you new to ansible?', :yellow
-  say_status  'answer', 'http://docs.ansible.com/intro_getting_started.html', :white
-  puts        '-'*80
+  say_status  'task', "#{method_to_sentence(message.to_s)}:", :yellow
+  puts        '-'*80, ''; sleep 0.25
 end
 
 def git_commit(message)
@@ -40,23 +38,32 @@ def git_config(field)
   git_field_value.to_s.empty? ? default_value : git_field_value
 end
 
-author_name = git_config 'name'
-author_email = git_config 'email'
+# ---
 
-log_task 'Remove all of the generated rails code'
-run 'rm -rf * .git .gitignore'
+def delete_generated_rails_code
+  log_task __method__
 
-log_task 'Add playbook directory'
-run "mkdir -p #{app_name}"
-run "mv #{app_name}/* ."
-run "rm -rf #{app_name}"
-git :init
-git_commit 'Initial commit'
+  run 'rm -rf * .git .gitignore'
+end
 
-log_task 'Add a licence'
-run 'rm -rf LICENSE'
+def add_playbook_directory
+  log_task __method__
 
-file 'LICENSE' do <<-S
+  run "mkdir -p #{app_name}"
+  run "mv #{app_name}/* ."
+  run "rm -rf #{app_name}"
+  git :init
+  git_commit 'Initial commit'
+end
+
+def add_license
+  log_task __method__
+
+  author_name = git_config 'name'
+  author_email = git_config 'email'
+
+  run 'rm -rf LICENSE'
+  file 'LICENSE' do <<-S
 The MIT License (MIT)
 
 Copyright (c) #{Time.now.year} #{author_name} <#{author_email}>
@@ -79,12 +86,15 @@ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
 CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-S
+  S
+  end
+  git_commit 'Add MIT license'
 end
-git_commit 'Add MIT license'
 
-log_task 'Add the main playbook'
-file 'site.yml' do <<-S
+def add_main_playbook
+  log_task __method__
+
+  file 'site.yml' do <<-S
 ---
 - name: ensure all servers are commonly configured
   hosts: all
@@ -138,12 +148,36 @@ file 'site.yml' do <<-S
     - { role: nickjj.pumacorn, tags: [app, rails] }
     - { role: nickjj.sidekiq, tags: [app, rails] }
     - { role: nickjj.monit, tags: [app, monit] }
-S
+  S
+  end
+  git_commit 'Add the main playbook'
 end
-git_commit 'Add the main playbook'
 
-log_task 'Remove unused files from git'
-git add: '-u'
-git_commit 'Remove unused files'
+def remove_unused_files_from_git
+  log_task __method__
 
+  git add: '-u'
+  git_commit 'Remove unused files'
+end
+
+def log_complete
+  puts
+  say_status  'success', "\e[1m\Everything has been setup successfully\e[0m", :cyan
+  puts
+  say_status  'question', 'Are most of your apps similar?', :yellow
+  say_status  'answer', 'You only need to generate one playbook and you just did', :white
+  say_status  'answer', 'Use the inventory in each project to customize certain things', :white
+  puts
+  say_status  'question', 'Are you new to ansible?', :yellow
+  say_status  'answer', 'http://docs.ansible.com/intro_getting_started.html', :white
+  puts        '-'*80
+end
+
+# ---
+
+delete_generated_rails_code
+add_playbook_directory
+add_license
+add_main_playbook
+remove_unused_files_from_git
 log_complete
