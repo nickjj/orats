1f3f035ba22e971a489f46e883c00a2544d7b01e
Nick Janetakis <nick.janetakis@gmail.com>
2014-06-19 14:08:15 -0400

    Add yaml syntax tests for the inventory and playbook

--- test/integration/cli_test.rb
+++ test/integration/cli_test.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require_relative '../test_helper'
 
 class TestCLI < Minitest::Test
@@ -27,13 +28,11 @@ class TestCLI < Minitest::Test
   end
 
   def test_play
-    @target_path = generate_app_name
-    assert_orats 'play', 'success'
+    assert_play
   end
 
   def test_diff
-    @target_path = generate_app_name
-    assert_orats 'play', 'success'
+    assert_play
 
     @target_path = ''
     assert_orats 'diff', 'Compare this version of'
@@ -45,21 +44,27 @@ class TestCLI < Minitest::Test
 
   private
 
-  def assert_new_app(flags = '', extras: nil)
-    @target_path = generate_app_name
-    @extra_flags = "#{ORATS_NEW_FLAGS} #{flags}"
-
-    assert_orats 'new', 'Start your server', extras: extras
-
-    @target_path << "/services/#{@target_path}" unless flags == '--skip-extras'
-    assert_app_tests_pass "#{TEST_PATH}/#{@target_path}"
-  end
-
   def assert_orats(command, match_regex, extras: nil)
     out, err = capture_orats(command)
 
     assert_match /#{match_regex}/, out
-    assert_or_refute_extras(extras) if extras
+
+    assert_or_refute_extras extras if extras
+  end
+
+  def assert_new_app(flags = '', extras: nil)
+    @target_path         = generate_app_name
+    absolute_target_path = "#{TEST_PATH}/#{@target_path}"
+    @extra_flags         = "#{ORATS_NEW_FLAGS} #{flags}"
+
+    assert_orats 'new', 'Start your server', extras: extras
+
+    if extras == :assert
+      assert_ansible_yaml "#{absolute_target_path}/inventory/group_vars/all.yml"
+      absolute_target_path << "/services/#{@target_path}"
+    end
+
+    assert_app_tests_pass absolute_target_path
   end
 
   def assert_app_tests_pass(target_path)
@@ -68,7 +73,13 @@ class TestCLI < Minitest::Test
     end
 
     log_rails_test_results out
-    assert out.include?('0 failures') && out.include?('0 errors')
+    assert out.include?('0 failures') && out.include?('0 errors'), err
+  end
+
+  def assert_play
+    @target_path = generate_app_name
+    assert_orats 'play', 'success'
+    assert_ansible_yaml "#{TEST_PATH}/#{@target_path}/site.yml"
   end
 
   def assert_nuked(options = {})
@@ -94,11 +105,29 @@ class TestCLI < Minitest::Test
   end
 
   def assert_or_refute_extras(assert_or_refute)
-    assert_path "#{TEST_PATH}/#{@target_path}"
+    absolute_target_path = "#{TEST_PATH}/#{@target_path}"
+
+    assert_path absolute_target_path
     send("#{assert_or_refute.to_s}_path",
-             "#{TEST_PATH}/#{@target_path}/inventory")
+         "#{absolute_target_path}/inventory")
     send("#{assert_or_refute.to_s}_path",
-             "#{TEST_PATH}/#{@target_path}/secrets")
+         "#{absolute_target_path}/secrets")
+  end
+
+  def assert_ansible_yaml(file_path)
+    begin
+      file                 = IO.read(file_path)
+      invalid_ansible_yaml = file.match(/^([^#].\w): {/)
+
+      if invalid_ansible_yaml
+        assert false, "Invalid yaml syntax found near:\n #{invalid_ansible_yaml}\n\nYou need to include quotes around values that start with jinja template tags:\n Example, foo: '{{ bar }}'"
+        return
+      end
+
+      assert YAML.load(file)
+    rescue Psych::SyntaxError => ex
+      assert false, ex.message
+    end
   end
 
   def capture_orats(command)
@@ -113,9 +142,9 @@ class TestCLI < Minitest::Test
     out_lines = out.split("\n")
 
     out_lines.delete_if do |line|
-      line.include?('Sidekiq') || line .start_with?('.') ||
-      line.include?('Running') || line.include?('Run options') ||
-      line.empty?
+      line.include?('Sidekiq') || line.start_with?('.') ||
+          line.include?('Running') || line.include?('Run options') ||
+          line.empty?
     end
 
     puts
