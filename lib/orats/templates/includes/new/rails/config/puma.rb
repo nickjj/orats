247f45ad32af38029857c0e228c6322b7668b478
Nick Janetakis <nick.janetakis@gmail.com>
2014-08-05 10:08:15 -0400

    Remove 1 too many levels of service directories

--- lib/orats/templates/includes/new/rails/config/puma.rb
+++ lib/orats/templates/includes/new/rails/config/puma.rb
@@ -6,17 +6,15 @@ workers ENV['WORKERS'].to_i
 if ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test'
   bind 'tcp://0.0.0.0:3000'
 else
-  bind "unix:#{ENV['RUN_STATE_PATH']}/#{ENV['SERVICE']}/#{ENV['SERVICE']}"
+  bind "unix:#{ENV['RUN_STATE_PATH']}/#{ENV['SERVICE']}"
 end
 
-pidfile "#{ENV['RUN_STATE_PATH']}/#{ENV['SERVICE']}/#{ENV['SERVICE']}.pid"
+pidfile "#{ENV['RUN_STATE_PATH']}/#{ENV['SERVICE']}.pid"
 
 worker_timeout 30
 
-stdout_redirect "#{ENV['LOG_PATH']}/#{ENV['SERVICE']}/" + \
-                "#{ENV['SERVICE']}.access.log",
-                "#{ENV['LOG_PATH']}/#{ENV['SERVICE']}/" + \
-                "#{ENV['SERVICE']}.error.log"
+stdout_redirect "#{ENV['LOG_PATH']}/#{ENV['SERVICE']}.access.log",
+                "#{ENV['LOG_PATH']}/#{ENV['SERVICE']}.error.log"
 
 preload_app!
 
