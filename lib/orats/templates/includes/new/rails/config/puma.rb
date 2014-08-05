ced475a6393abfde779ab598129a83fbe82a8450
Nick Janetakis <nick.janetakis@gmail.com>
2014-08-05 19:25:06 -0400

    Add comments to both server configs

--- lib/orats/templates/includes/new/rails/config/puma.rb
+++ lib/orats/templates/includes/new/rails/config/puma.rb
@@ -1,26 +1,39 @@
+# What environment should the server be started under?
 environment ENV['RAILS_ENV']
 
+# Feel free to experiment with this, 0/16 is a good starting point.
 threads ENV['THREADS_MIN'].to_i, ENV['THREADS_MAX'].to_i
+
+# Go with at least 1 per CPU core, a higher amount will usually help for fast
+# responses such as reading from a cache.
 workers ENV['WORKERS'].to_i
 
+# This allows you to develop on port 3000 while using a socket elsewhere.
 if ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test'
   bind 'tcp://0.0.0.0:3000'
 else
   bind "unix:#{ENV['RUN_STATE_PATH']}/#{ENV['SERVICE']}"
 end
 
+# The path where the pid file will be written to.
 pidfile "#{ENV['RUN_STATE_PATH']}/#{ENV['SERVICE']}.pid"
 
+# Use a shorter timeout instead of the 60s default. If you are handling large
+# uploads you may want to increase this.
 worker_timeout 30
 
+# The paths to where logs will be written to.
 stdout_redirect "#{ENV['LOG_PATH']}/#{ENV['SERVICE']}.access.log",
                 "#{ENV['LOG_PATH']}/#{ENV['SERVICE']}.error.log"
 
+# Preload the application before starting the workers.
 preload_app!
 
+# The command used to issue a restart.
 restart_command 'bundle exec puma'
 
 on_worker_boot do
+  # Don't bother having the master process hang onto older connections.
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 
--- lib/orats/templates/includes/new/rails/config/unicorn.rb
+++ lib/orats/templates/includes/new/rails/config/unicorn.rb
@@ -1,30 +1,46 @@
-# heavily inspired by gitlab:
+# Heavily inspired by gitlab:
 # https://github.com/gitlabhq/gitlabhq/blob/master/config/unicorn.rb.example
 
+# Go with at least 1 per CPU core, a higher amount will usually help for fast
+# responses such as reading from a cache.
 worker_processes ENV['WORKERS'].to_i
 
+# This allows you to develop on port 3000 while using a socket elsewhere.
 if ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test'
   listen '0.0.0.0:3000'
 else
+  # A shorter backlog will force quicker fails when the server is busy.
   listen "unix:#{ENV['RUN_STATE_PATH']}/#{ENV['SERVICE']}",
-         backlog: 64
+  backlog: 64
 end
 
+# The path where the pid file will be written to.
 pid "#{ENV['RUN_STATE_PATH']}/#{ENV['SERVICE']}.pid"
 
+# Use a shorter timeout instead of the 60s default. If you are handling large
+# uploads you may want to increase this.
 timeout 30
 
+# The paths to where logs will be written to.
 stdout_path "#{ENV['LOG_PATH']}/#{ENV['SERVICE']}.access.log"
 stderr_path "#{ENV['LOG_PATH']}/#{ENV['SERVICE']}.error.log"
 
+# Combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings:
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
 preload_app true
-
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
 
+# Enable this flag to have unicorn test client connections by writing the
+# beginning of the HTTP headers before calling the application. This
+# prevents calling the application for connections that have disconnected
+# while queued. This is only guaranteed to detect clients on the same
+# host unicorn runs on, and unlikely to detect disconnects even on a
+# fast LAN.
 check_client_connection false
 
 before_fork do |server, worker|
+  # Don't bother having the master process hang onto older connections.
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 
@@ -45,9 +61,25 @@ before_fork do |server, worker|
     rescue Errno::ENOENT, Errno::ESRCH
     end
   end
+
+  # Throttle the master from forking too quickly by sleeping. Due
+  # to the implementation of standard Unix signal handlers, this
+  # helps (but does not completely) prevent identical, repeated signals
+  # from being lost when the receiving process is busy.
+  # sleep 1
 end
 
 after_fork do |server, worker|
+  # Per-process listener ports for debugging, admin, migrations, etc..
+  # addr = "127.0.0.1:#{9293 + worker.nr}"
+  # server.listen(addr, tries: -1, delay: 5, tcp_nopush: true)
+
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
+
+  # If preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis. TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls).
 end
