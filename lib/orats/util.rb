88fb7e3485bc5fd0c9f1762e214f1c4c3abc2e2f
Michał Zając <michal.zajac@gmail.com>
2018-09-26 18:26:35 +0200

    Util#underscore(term) is no longer destructive

--- lib/orats/util.rb
+++ lib/orats/util.rb
@@ -3,11 +3,11 @@
 class Util
   def self.underscore(term)
     # taken from ActiveSupport
-    term.gsub!(/::/, '/')
-    term.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-    term.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-    term.tr!('-', '_')
-    term.downcase
+    term.gsub(/::/, '/')
+        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+        .tr('-', '_')
+        .downcase
   end
 
   def self.classify(term)
